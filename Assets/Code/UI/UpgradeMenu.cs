ef1c5d8507775c36404283b2b721addc6551608b
Franklin.J.Shedleski <fshedleski@gmail.com>
2023-03-21 00:40:35 -0400

    start of menu. how to make panel work?

---
+++ Assets/Code/UI/UpgradeMenu.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeMenu : MonoBehaviour
+{
+    // pointers
+    private Camera mainCam = null;
+
+    // mouse click debounce
+    private bool debounceIsMouseDown = false;
+    private int debounceCounter = 0;
+    private const int debounceFrames = 3;
+
+    // menu object and component pointers
+    GameObject UpgradeMenuRoot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = GameObject.Find("MainCamera").GetComponent<Camera>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(debounceIsMouseDown)
+        {
+            debounceCounter++;
+            if(debounceCounter >= debounceFrames)
+            {
+                debounceCounter = 0;
+                debounceIsMouseDown = false;
+            }
+        } 
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            { // if left button pressed...
+                debounceIsMouseDown = true;
+
+                Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    UpgradeManagerBase umb = hit.collider.GetComponentInParent<UpgradeManagerBase>();
+                    if (umb != null)
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
