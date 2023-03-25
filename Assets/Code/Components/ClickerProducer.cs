c3194add7568cdb28b5d29d67dfa084441e19380
Cameron James Perrie <cperrie@umd.edu>
2023-03-25 12:58:44 -0400

    bug fix upgrades not being when clicked

--- Assets/Code/Components/ClickerProducer.cs
+++ Assets/Code/Components/ClickerProducer.cs
@@ -19,13 +19,14 @@ public class ClickerProducer : ProducerBase
     protected override double Tick(){
         double ProductionThisTick = 0;
 
-        // Production from Upgrades
-        if (UMptr != null)
-        {
-            ProductionThisTick += UMptr.Tick();
-        }
+       
         //If the object was clicked this tick, then generate the click income
         if (clicked){
+             // Production from Upgrades
+            if (UMptr != null)
+            {
+                ProductionThisTick += UMptr.Tick();
+            }
             ProductionThisTick += BaseProduction;
             clicked = false;
         }
